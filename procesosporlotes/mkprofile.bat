6c53d2718a7fb48b55adb1934902115222696c7c
eleonr08 <150673505+eleonr08@users.noreply.github.com>
2023-11-14 12:38:37 +0100

    mkprofile.dir

---
+++ procesosporlotes/mkprofile.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Crea un script dentro del perfil de usuario actual que cree los directorios asir daw ceti 
+REM Autor: Elias Leon RIvera
+REM Fecha: 13-11-2023
+
+
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\smr %USERPROFILE%\daw %USERPROFILE%\ceti
+mkdir %USERPROFILE%\asir\prac %USERPROFILE%\asir\docts %USERPROFILE%\asir\exam
+mkdir %USERPROFILE%\dam\prac %USERPROFILE%\dam\docts %USERPROFILE%\dam\exam
+mkdir %USERPROFILE%\smr\prac %USERPROFILE%\smr\docts %USERPROFILE%\smr\exam
+mkdir %USERPROFILE%\daw\prac %USERPROFILE%\daw\docts %USERPROFILE%\daw\exam
+mkdir %USERPROFILE%\ceti\prac %USERPROFILE%\ceti\docts %USERPROFILE%\ceti\exam
+
